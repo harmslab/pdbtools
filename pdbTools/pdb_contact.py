5cafd401190844a65d99a5b543bcb334ead41b55
harmsm <harmsm@15b25b17-7346-0410-a45f-775d9c02966d>
2012-11-21 17:55:41 +0000

    Reorganize the source tree a bit.  Add some code to find close contacts.

---
+++ pdbTools/pdb_contact.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+# Copyright 2008, Michael J. Harms
+# This program is distributed under General Public License v. 3.  See the file
+# COPYING for a copy of the license.  
+
+__description__ = \
+"""
+pdb_contacting-residues.py
+
+Find all residues within some cutoff of a target residue in a pdb file.
+"""
+__author__ = "Michael J. Harms"
+__date__ = "111116"
+
+import os
+
+def pdbContacting(pdb,target,cutoff):
+    """
+    """
+  
+    cutoff_sq = (cutoff)**2
+
+    to_take = ["ATOM  ","HETATM"] 
+    all_coord = [[l[12:26],[float(l[30+8*i:38+8*i]) for i in range(3)]]
+                 for l in pdb if l[0:6] in to_take] 
+
+    target_list = [a for a in all_coord if a[0][5:8].strip() == target]
+
+    out = []
+    for t in target_list:
+        contacts = []
+        for a in all_coord:
+            if sum([(a[1][i]-t[1][i])**2 for i in range(3)]) < cutoff:
+
+                # ignore self
+                if t[0] == a[0]:
+                    continue
+                contacts.append(a[0][5:].strip())
+      
+        # Grab only unique contacts 
+        contacts = dict([(c,()) for c in contacts]).keys() 
+        out.append("%s\t%s\n" % (t[0],("\t".join(contacts))))
+   
+    return out 
+
+def main():
+    """
+    Function to call if run from command line.
+    """
+
+    from helper import cmdline
+    import pdb_splitnmr
+
+    
+    cmdline.initializeParser(__description__,__date__)
+    cmdline.addOption(short_flag="t",
+                      long_flag="target",
+                      action="store",
+                      default="TRP",
+                      help="residue target to look for contacts near",
+                      nargs=1)
+    cmdline.addOption(short_flag="d",
+                      long_flag="distance",
+                      action="store",
+                      default=3.5,
+                      help="distance cutoff for calling contacts",
+                      nargs=1,
+                      type=float)
+
+    file_list, options = cmdline.parseCommandLine()
+
+    out = []
+    for pdb_file in file_list:
+        
+        f = open(pdb_file,'r')
+        pdb = f.readlines()
+        f.close()
+
+        # Only take the first NMR model
+        models = pdb_splitnmr.splitNMR(pdb)
+        if len(models) > 0:
+            pdb = models[0]
+
+        tmp_out = pdbContacting(pdb,options.target,options.distance)
+
+        out.extend(["%s\t%s" % (pdb_file[:-4],t) for t in tmp_out])
+
+    return out
+
+
+# If run from command line...
+if __name__ == "__main__":
+    
+    out = main()
+    print "".join(out)
+        
+
