149723836943aad611dcf86b119cf5e79372c431
harmsm <harmsm@15b25b17-7346-0410-a45f-775d9c02966d>
2014-04-07 15:50:54 +0000

    Altered pdb_splitnmr so it now names each file with the model number stored in the file, rather than a simple sequential index starting with 0.  Thanks to L. Sanchez for the suggestion.

--- pdbTools/pdb_splitnmr.py
+++ pdbTools/pdb_splitnmr.py
@@ -22,13 +22,15 @@ def splitNMR(pdb):
 
     to_strip = ["ENDMDL","MASTER"]
     pdb = [l for l in pdb if l[0:6] not in to_strip]
-    pdb_hash = [(l[0:6],i) for i, l in enumerate(pdb)]
-    pdb_hash = [x[1] for x in pdb_hash if x[0] == "MODEL "]
-    pdb_hash.append(len(pdb))
+    pdb_splitter = [(l[0:6],i) for i, l in enumerate(pdb)]
+    pdb_splitter = [x[1] for x in pdb_splitter if x[0] == "MODEL "]
+    model_numbers = [pdb[i].split()[1].strip() for i in pdb_splitter]
+    pdb_splitter.append(len(pdb))
 
     all_models = []
-    for i in range(1,len(pdb_hash)):
-        all_models.append(pdb[pdb_hash[i-1]:pdb_hash[i]])
+    for i in range(1,len(pdb_splitter)):
+        all_models.append((model_numbers[i-1],
+                           pdb[pdb_splitter[i-1]:pdb_splitter[i]]))
        
     return all_models 
 
@@ -50,9 +52,11 @@ def main():
         models = splitNMR(pdb)
         
         short_pdb = os.path.split(pdb_file)[-1][:-4]
-        for index, model in enumerate(models):
-            g = open("%s_%i.pdb" % (short_pdb,index),"w")
-            g.writelines(model)
+        for model in models:
+            index = model[0]
+            pdb_lines = model[1]
+            g = open("%s_%s.pdb" % (short_pdb,index),"w")
+            g.writelines(pdb_lines)
             g.close()
 
 if __name__ == "__main__":
